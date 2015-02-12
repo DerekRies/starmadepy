7feeb3d185af89730e71e293b3e62cb33bef76d5
Derek Ries <influenztial@gmail.com>
2015-02-11 20:52:46 -0800

    installed verison working as well as local

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.2',
+    version='0.1.4',
 
     description='An API for reading and manipulating Starmade game data',
     long_description=long_description,
@@ -65,8 +65,9 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
+    include_package_data=True,
     package_data={
-        'data': ['data/items-complete.json'],
+        'starmadepy': ['data/items-complete.json'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
